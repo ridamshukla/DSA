82e41ce44a09611d1845be094b732c8bd0bd7e20
ridamshukla <rishuridamshukla@gmail.com>
2025-01-10 18:15:12 +0530

    Sorting String

--- String/SortingString.java
+++ String/SortingString.java
@@ -1,4 +1,27 @@
 package String;
 
 public class SortingString {
+    public static void main(String[] args){
+        StringBuilder s = new StringBuilder("Ridam shukla");
+
+        int i = 97;
+        while(i <= 122){
+            int j = 0;
+            while(j < s.length()){
+                if(s.charAt(j) == ' '){
+                    j++;
+                }
+                int a = (i-32);
+                char ch = (char)i;
+                char ch1 = (char)a;
+                if(s.charAt(j) == ch){
+                    System.out.print(ch);
+                } else if(s.charAt(j) == ch1){
+                    System.out.print(ch1);
+                }
+                j++;
+            }
+            i++;
+        }
+    }
 }
