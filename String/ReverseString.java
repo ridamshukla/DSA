afdb564ee6cb7f53a6c13e0ec75bef1e23ba7d51
ridamshukla <rishuridamshukla@gmail.com>
2025-01-10 18:14:10 +0530

    Practice String

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -1,4 +1,16 @@
 package String;
 
 public class ReverseString {
+    public static void main(String[] args){
+        String s = "Ridam";
+        StringBuilder r = new StringBuilder();
+
+        int b = s.length()-1;
+        while(b >= 0){
+            char ch1 = s.charAt(b);
+            r.append(ch1);
+            b--;
+        }
+        System.out.println(r);
+    }
 }
