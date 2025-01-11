4cfb2025e7fd2b1ddb0bf9e103b7be0e6740e2fa
ridamshukla <rishuridamshukla@gmail.com>
2025-01-11 19:35:35 +0530

    SetIthBit

--- BitManipulation/SetIthBit.java
+++ BitManipulation/SetIthBit.java
@@ -1,4 +1,14 @@
 package BitManipulation;
 
 public class SetIthBit {
+    public static void main(String[] args){
+        int num = 10;
+        int set = 2;
+        int bitmask = 1<<set;
+
+         num = num | bitmask;
+        System.out.println(num);
+
+
+    }
 }
