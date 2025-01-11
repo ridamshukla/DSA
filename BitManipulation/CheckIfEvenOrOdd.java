365ad7586c11f974ff3b027ef024f08e46b403bc
ridamshukla <rishuridamshukla@gmail.com>
2025-01-11 19:34:16 +0530

    CheckIfEvenOrOdd

--- BitManipulation/CheckIfEvenOrOdd.java
+++ BitManipulation/CheckIfEvenOrOdd.java
@@ -1,4 +1,14 @@
 package BitManipulation;
 
 public class CheckIfEvenOrOdd {
+    public static void main(String[] args){
+        int num = 3;
+        int bitmask = 1;
+
+        if((num & bitmask) == 0){
+            System.out.println("Even Number: ");
+        } else{
+            System.out.println("Odd Number: ");
+        }
+    }
 }
