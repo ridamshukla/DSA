3b3b7bb1bfb672529a3573870f85d9f7092890f3
ridamshukla <rishuridamshukla@gmail.com>
2025-01-11 19:33:16 +0530

    BinaryRightShiftOperator

--- BitManipulation/BinaryRightShiftOperator.java
+++ BitManipulation/BinaryRightShiftOperator.java
@@ -1,4 +1,18 @@
 package BitManipulation;
 
 public class BinaryRightShiftOperator {
+    public static void main(String[] args){
+        int num = 5;
+        int num1 = 2;
+
+        System.out.println(num >> num1);
+
+        /*
+        iska answer 1 aayega kuiki 5 ka binary 101 hota he ab isko right shift karenge to
+        right ki 2 digit hat jayegi aage 0 add ho jayenge 001
+        001 ka decimal 1 hota he iska shortcut formula hota he a / (2 * num1)
+
+         */
+
+    }
 }
