5819b7b3a14aee0a04e53c62046c23c12eb5aef8
ridamshukla <rishuridamshukla@gmail.com>
2025-01-11 19:32:09 +0530

    BinaryLeftShiftOperator

--- BitManipulation/BinaryLeftShiftOperator.java
+++ BitManipulation/BinaryLeftShiftOperator.java
@@ -1,4 +1,17 @@
 package BitManipulation;
 
 public class BinaryLeftShiftOperator {
+    public static void main(String[] args){
+        int num = 5;
+        int num1 = 8;
+
+        System.out.println(num << num1);
+
+        // iska answer aayega 20 kyuki binary left shift operator ese kam karta he
+        // 5 ka binary hota he 101 or vo memory me 000101 ese store hota he
+        // Ab isko 2 time left shift karna he to iske left ke 00 hat jayenge
+        // or ye ese store hoga 010100 aage jo space bacha usme 0 add ho jayenge
+        // ab ye ese calculate hota he 10100 to  20 aayega
+        // iska simple formula hota he (a * (2 * b))
+    }
 }
