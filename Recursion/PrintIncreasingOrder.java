4768646d765202e7074aeb58dfef5c914090f701
ridamshukla <rishuridamshukla@gmail.com>
2025-01-16 18:02:50 +0530

    PrintIncresingOrder

--- Recursion/PrintIncreasingOrder.java
+++ Recursion/PrintIncreasingOrder.java
@@ -1,4 +1,16 @@
 package Recursion;
 
 public class PrintIncreasingOrder {
+    public static void main(String[] args){
+        int num = 10;
+        PrintDecreasingorder(num);
+    }
+    public static void PrintDecreasingorder(int num){
+        if(num == 1){
+            System.out.println(num);
+            return;
+        }
+        PrintDecreasingorder(num-1);
+        System.out.println(num);
+    }
 }
