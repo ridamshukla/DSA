f5575ce3a3ba2a002b70a3f74f4dbd6a4ec18275
ridamshukla <rishuridamshukla@gmail.com>
2025-01-16 12:22:40 +0530

    GetIthBit

--- BitManipulation/GetIthBit.java
+++ BitManipulation/GetIthBit.java
@@ -4,21 +4,21 @@ public class GetIthBit {
     public static void main(String[] args){
         byte num = 0b1011;
         System.out.println(num);
-//        int num1 = num >> 2;
-//        System.out.println(num1);
-//        int find = 2;
-//        int bitmask = 1 << find;
-//
-//        // first step by left shift operator by using 1
-////        if((num & bitmask) == 0){
-////            System.out.println("number is 0");
-////        } else {
-////            System.out.println("number is 1");
-////        }
-//        if((num1 & 1) == 0){
-//            System.out.println("number is 0");
-//        } else {
-//            System.out.println("number is 1");
-//        }
+        int num1 = num >> 2;
+        System.out.println(num1);
+        int find = 2;
+        int bitmask = 1 << find;
+
+//         first step by left shift operator by using 1
+        if((num & bitmask) == 0){
+            System.out.println("number is 0");
+        } else {
+            System.out.println("number is 1");
+        }
+        if((num1 & 1) == 0){
+            System.out.println("number is 0");
+        } else {
+            System.out.println("number is 1");
+        }
     }
 }
