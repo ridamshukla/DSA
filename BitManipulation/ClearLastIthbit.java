2da805e0dd6570300246c82dac2fb7f51b08dd57
ridamshukla <rishuridamshukla@gmail.com>
2025-01-16 12:19:02 +0530

    ClearLastIthBit

--- BitManipulation/ClearLastIthbit.java
+++ BitManipulation/ClearLastIthbit.java
@@ -1,4 +1,12 @@
 package BitManipulation;
 
 public class ClearLastIthbit {
+    public static void main(String[] args){
+        int num = 14;
+        int pos = 2;
+        int num1 = (~0 << pos);
+        int ans = (num & num1);
+
+        System.out.println(ans);
+    }
 }
