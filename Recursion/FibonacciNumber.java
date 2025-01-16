661280df18915267bc76acecb11a10a811188b7b
ridamshukla <rishuridamshukla@gmail.com>
2025-01-16 17:59:23 +0530

    FibonacciNumber

--- Recursion/FibonacciNumber.java
+++ Recursion/FibonacciNumber.java
@@ -1,4 +1,16 @@
 package Recursion;
 
 public class FibonacciNumber {
+    public static void main(String[] args){
+        int num = 5;
+        fibonacci(num);
+    }
+    public static void fibonacci(int num){
+        int ans = 0;
+        System.out.println("0 1 ");
+        if(num == 0){
+            return;
+        }
+        fibonacci(num-1);
+    }
 }
