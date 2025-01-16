1a2c02d025808afd6cacd7191a2bc88d0a1eac21
ridamshukla <rishuridamshukla@gmail.com>
2025-01-16 12:19:43 +0530

    ClearRangeOfBits

--- BitManipulation/ClearRangeOfBits.java
+++ BitManipulation/ClearRangeOfBits.java
@@ -1,4 +1,11 @@
 package BitManipulation;
 
 public class ClearRangeOfBits {
+    public static void main(String[] args){
+        int num = 43;
+        int bitmask = 1;
+        int num1 = (bitmask << 5);
+        int ans = (num & num1);
+        System.out.println(ans);
+    }
 }
