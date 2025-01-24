c421ccc119d2f3f1c6f037dac5e3bfdb409d84c7
ridamshukla <rishuridamshukla@gmail.com>
2025-01-24 18:16:52 +0530

    RemoveDuplicateFromSortedList

--- LinkedList/RemoveDuplicateFromSortedList.java
+++ LinkedList/RemoveDuplicateFromSortedList.java
@@ -1,4 +1,50 @@
 package LinkedList;
 
+class r{
+    Node head;
+    Node tail;
+
+    void Insert(int data){
+        Node temp = new Node(data);
+
+        if(head == null) head = tail = temp;
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+    }
+
+    void Remove(){
+        Node temp = head;
+        while(temp.next != null && temp != null){
+            if(temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else{
+                temp = temp.next;
+            }
+        }
+    }
+
+    void Display(){
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}
 public class RemoveDuplicateFromSortedList {
+    public static void main(String[] args){
+        r a = new r();
+        a.Insert(1);
+        a.Insert(1);
+        a.Insert(2);
+        a.Insert(2);
+        a.Insert(3);
+        a.Insert(4);
+        a.Insert(5);
+        a.Insert(6);
+        a.Remove();
+        a.Display();
+    }
 }
