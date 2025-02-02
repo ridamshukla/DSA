61ab8df23e82ad7863cfcd4986c4ab3458e8182b
ridamshukla <rishuridamshukla@gmail.com>
2025-02-02 16:50:19 +0530

    Important Concept Of Hexdecimal

---
+++ Arrays/ImportantConcept.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class ImportantConcept {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,012,4};
+
+
+        // 0 se start hone vala number hexadecimal ki tarah treat kiye jate he isliye 10 aayega answer
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+
+}
