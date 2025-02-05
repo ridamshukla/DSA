c02f571c70d888ed68007116947d6b3d6920b091
ridamshukla <rishuridamshukla@gmail.com>
2025-02-05 15:14:00 +0530

    declaration of stack

---
+++ Stack/BasicStack.java
@@ -0,0 +1,12 @@
+package Stack;
+
+import java.util.Stack;
+
+public class BasicStack {
+    public  static void main(String[] args){
+        Stack<Integer> s = new Stack<>();
+        // this is declaration of stack
+
+    }
+
+}
